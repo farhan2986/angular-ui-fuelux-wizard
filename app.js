3de02792226c45a774e3ffb840425473b81acc3d
Farhan Shahid <farhan2986@hotmail.com>
2015-03-18 12:52:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 var app = angular.module('demoApp', ['fuelux.wizard']);
 
 app.controller('WizardController', ['$scope', function($scope){
-
+  $scope.stepCurrent = 2;	
   $scope.log = function(event){
     console.log(event);
     //if(event.direction=='next') return true;
   }
-
+  $scope.$watch('stepCurrent', function(stepValue) {
+      console.log("stepCurrent updated  ", stepValue, $scope.stepCurrent);
+  });
   $scope.user = {};
 
 }]);
