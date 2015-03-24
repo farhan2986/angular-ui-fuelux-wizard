e19de45d58803ece39d3bc8b9d0f8fea8e774dda
Farhan Shahid <farhan2986@hotmail.com>
2015-03-24 17:34:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 
+
 var app = angular.module('demoApp', ['fuelux.wizard']);
 
-app.controller('WizardController', ['$scope', function($scope){
-  $scope.stepCurrent = 2;	
+app.controller('WizardController', ['$scope','$interval', function($scope, $interval){
+  $scope.stepCurrent = 0;	
   $scope.log = function(event){
     console.log(event);
     //if(event.direction=='next') return true;
   }
+  $interval(function(){$scope.stepCurrent = Math.floor(Math.random(0, 5)*5);}, 2000);
+  
   $scope.$watch('stepCurrent', function(stepValue) {
       console.log("stepCurrent updated  ", stepValue, $scope.stepCurrent);
   });
-  $scope.user = {};
-
 }]);
